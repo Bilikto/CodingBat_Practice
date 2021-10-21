55870e5e256ef3ede3f44d5a46da8b6b5726017c
Bilikto <dashievbilikto@gmail.com>
2021-10-21 11:28:07 -0500

    compare last digits

---
+++ src/com/company/LastDigit.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class LastDigit {
+    public static void main(String[] args) {
+        /*
+        * Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+        * Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+        * lastDigit(7, 17) → true
+        * lastDigit(6, 17) → false
+        * lastDigit(3, 113) → true
+        * */
+
+        System.out.println(lastDigit(7, 17));
+        System.out.println(lastDigit(6, 17));
+        System.out.println(lastDigit(3, 113));
+    }
+
+    public static boolean lastDigit(int a, int b) {
+//        int lastDigitA = a % 10;
+//        int lastDigitB = b % 10;
+//        if (lastDigitA  == lastDigitB) {
+//            return true;
+//        }
+//        return false;
+
+        return (a % 10 ==  b % 10);
+    }
+}
