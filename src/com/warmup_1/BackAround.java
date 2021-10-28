b568296baedeb0fbb05cbbd1270522fa2730cea8
Bilikto <dashievbilikto@gmail.com>
2021-10-27 23:19:44 -0500

    return a new string with the last char added at the front and back

---
+++ src/com/warmup_1/BackAround.java
@@ -0,0 +1,27 @@
+package com.warmup_1;
+
+public class BackAround {
+    public static void main(String[] args) {
+        /*
+        * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt".
+        * The original string will be length 1 or more.
+
+        * backAround("cat") → "tcatt"
+        * backAround("Hello") → "oHelloo"
+        * backAround("a") → "aaa"
+        * */
+
+        System.out.println(backAround("cat"));
+        System.out.println(backAround("Hello"));
+        System.out.println(backAround("a"));
+    }
+
+    public static String backAround(String str) {
+
+        char char1 = str.charAt(str.length() -1);
+        return char1 + str + char1;
+
+
+
+    }
+}
