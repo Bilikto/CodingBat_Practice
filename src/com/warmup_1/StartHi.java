5d46ba3700000f7e0a250d1c0510eb5ed8d65d8b
Bilikto <dashievbilikto@gmail.com>
2021-10-30 21:42:34 -0500

    return true if the string starts with hi

---
+++ src/com/warmup_1/StartHi.java
@@ -0,0 +1,20 @@
+package com.warmup_1;
+
+public class StartHi {
+    public static void main(String[] args) {
+        /*
+        * Given a string, return true if the string starts with "hi" and false otherwise.
+        * startHi("hi there") → true
+        * startHi("hi") → true
+        * startHi("hello hi") → false
+        * */
+
+        System.out.println(startHi("hi there"));
+        System.out.println(startHi("hi"));
+        System.out.println(startHi("hello hi"));
+    }
+
+    public static boolean startHi(String str) {
+        return str.startsWith("hi");
+    }
+}
