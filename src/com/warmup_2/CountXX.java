ae660dbd46e7490efd8627f0b0ea40c2f6ac1b89
Bilikto <dashievbilikto@gmail.com>
2021-11-15 17:19:06 -0600

    fixed

--- src/com/warmup_2/CountXX.java
+++ src/com/warmup_2/CountXX.java
@@ -3,7 +3,7 @@ package com.warmup_2;
 public class CountXX {
 //    public static void main(String[] args) {
 //        /*
-//        * Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+//        * git .
 //        *
 //        * countXX("abcxx") → 1
 //        * countXX("xxx") → 2
