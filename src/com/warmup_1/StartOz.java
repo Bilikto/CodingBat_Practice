ab1bf599910363d0abb3bc18042caeb130e3728b
Bilikto <dashievbilikto@gmail.com>
2021-11-20 09:59:06 -0600

    return a string made of the first 2 chars of 'oz' (if present)

---
+++ src/com/warmup_1/StartOz.java
@@ -0,0 +1,37 @@
+package com.warmup_1;
+
+public class StartOz {
+    public static void main(String[] args) {
+        /*
+        * Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and
+        * include the second only if it is 'z', so "ozymandias" yields "oz".
+        * startOz("ozymandias") → "oz"
+        * startOz("bzoo") → "z"
+        * startOz("oxx") → "o"
+        * */
+
+        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("bzoo"));
+        System.out.println(startOz("oxx"));
+        System.out.println(startOz("abc"));
+        System.out.println(startOz("o"));
+        System.out.println(startOz(""));
+    }
+
+    public static  String startOz(String str) {
+        String s = "";
+
+        if (str.length() >= 1 && str.charAt(0) == 'o') {
+            s = s + str.charAt(0);
+        }
+
+        if (str.length() >= 2 && str.charAt(1) == 'z') {
+            s =  s + str.charAt(1);
+        }
+
+        return s;
+    }
+
+
+
+}
--- src/com/warmup_2/Last2.java
+++ src/com/warmup_2/Last2.java
@@ -4,7 +4,6 @@ public class Last2 {
     public static void main(String[] args) {
 
         /*
-        *
         * Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string,
         * so "hixxxhi" yields 1 (we won't count the end substring).
         * last2("hixxhi") → 1
