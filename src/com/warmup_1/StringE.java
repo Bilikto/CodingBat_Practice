ecf0f3703e343ee47c58f962e7fdadcc57d83d9c
Bilikto <dashievbilikto@gmail.com>
2021-11-23 17:59:26 -0600

    Return true if the given string contains between 1 and 3 'e' chars

---
+++ src/com/warmup_1/StringE.java
@@ -0,0 +1,38 @@
+package com.warmup_1;
+
+public class StringE {
+
+    public static void main(String[] args) {
+        /*
+        * Return true if the given string contains between 1 and 3 'e' chars.
+        * stringE("Hello") → true
+        * stringE("Heelle") → true
+        * stringE("Heelele") → false
+        * */
+
+        System.out.println(stringE("Hello"));
+        System.out.println(stringE("Heelle"));
+        System.out.println(stringE("Heelele"));
+        System.out.println(stringE("e"));
+        System.out.println(stringE("a"));
+        System.out.println(stringE(""));
+    }
+
+    public static boolean stringE(String str) {
+
+        int count = 0;
+        for (int i = 0; i <= str.length() - 1; i++) {
+            if (str.charAt(i) == 'e') {
+                count++;
+            }
+        }
+
+        if (count >= 1 && count <= 3) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
