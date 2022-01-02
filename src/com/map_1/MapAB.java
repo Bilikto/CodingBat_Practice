deace4e5c422985c3d44ab0258a08afdd3732bb5
Bilikto Dashiev <dashievbilikto@gmail.com>
2022-01-01 19:41:56 -0600

    Given str and word. If str starts with word..

--- src/com/map_1/MapAB.java
+++ src/com/map_1/MapAB.java
@@ -7,7 +7,8 @@ public class MapAB {
 
     public static void main(String[] args) {
         /*
-        * Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys. If both keys are present, append their 2 string values together and store the result under the key "ab".
+        * Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys.
+        * If both keys are present, append their 2 string values together and store the result under the key "ab".
         * mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}
         * mapAB({"a": "Hi"}) → {"a": "Hi"}
         * mapAB({"b": "There"}) → {"b": "There"}
